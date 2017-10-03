a722547056956bcc8ec309632bc675bcdb732fe7
BrinkerVII <brinkervii@gmail.com>
2017-10-03 12:53:46 +0200

    Complete connect method on DaemonClient

--- src/daemon-client.ts
+++ src/daemon-client.ts
@@ -1,23 +1,55 @@
-import * as request from 'request';
+import * as requestPromise from 'request-promise';
 
 export class DaemonClient {
 	private clientToken: string;
 
 	constructor(
-		private baseURL: string
+		private baseURL: string,
+		private name: string = "Sync Apparatus FSClient"
 	) {
-
+		if (!this.baseURL.endsWith("/")) {
+			this.baseURL += "/";
+		}
 	}
 
 	public connect(): Promise<void> {
 		return new Promise((resolve, reject) => {
-			request(`${this.baseURL}/announce`, (err, response, body) => {
-				console.log(err);
-				console.log(response);
-				console.log(body);
-				
-				resolve();
-			});
+			if (this.isConnected()) {
+				return reject(new Error("Cannot connect because the client is already connected"));
+			}
+			
+			let options: requestPromise.Options = {
+				method: "POST",
+				uri: `${this.baseURL}announce`,
+				body: {
+					name: this.name
+				},
+				json: true
+			};
+
+			requestPromise(options)
+				.then(body => {
+					let doErorr = (errString: string) => {
+						reject(new Error(errString));
+					}
+
+					if (body) {
+						let clientId: string = body.clientId;
+						if (typeof clientId === "string") {
+							this.clientToken = clientId;
+							resolve();
+						} else {
+							doErorr("Call returned invalid type for clientId");
+						}
+					} else {
+						doErorr("No body returned");
+					}
+				})
+				.catch(reject);
 		});
+	}
+
+	public isConnected(): boolean {
+		return !!this.clientToken;
 	}
 }
